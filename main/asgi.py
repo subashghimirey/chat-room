f576f94238be6cf99bde464237af4a720e30deb0
Subash <ghimiresubash980843@gmail.com>
2024-05-11 00:08:50 +0545

    first version

---
+++ main/asgi.py
@@ -0,0 +1,24 @@
+
+import os
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
+
+django_asgi_app = get_asgi_application()
+
+from chat_app.routing import websocket_urlpatterns
+
+
+application = ProtocolTypeRouter(
+    {
+        "http" : django_asgi_app,
+        "websocket" : AllowedHostsOriginValidator(AuthMiddlewareStack(URLRouter(websocket_urlpatterns)))
+    }
+
+
+
+)
